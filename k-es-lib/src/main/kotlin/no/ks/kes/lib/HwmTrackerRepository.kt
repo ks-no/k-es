0dd99bd85446ee2a4149d7d7eda3da70b7dd16b8
Sondre Eikanger Kvalø <zapodot@gmail.com>
2020-10-29 08:13:34 +0100

    Allows getting hwm value without running init

--- k-es-lib/src/main/kotlin/no/ks/kes/lib/HwmTrackerRepository.kt
+++ k-es-lib/src/main/kotlin/no/ks/kes/lib/HwmTrackerRepository.kt
@@ -1,6 +1,7 @@
 package no.ks.kes.lib
 
 interface HwmTrackerRepository {
+    fun current(subscriber: String): Long?
     fun getOrInit(subscriber: String): Long
     fun update(subscriber: String, hwm: Long)
 }
