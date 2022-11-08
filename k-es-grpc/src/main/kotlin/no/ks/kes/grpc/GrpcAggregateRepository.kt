3cb6dda5b6fce09598f5a74caeac89577d905072
Anders Flemmen <anders.flemmen@dfind.no>
2022-11-08 15:45:07 +0100

    Ignorerer messages uten event (#435)
    * Ignorerer ReadMessages som ikke har event. Repository cleanup.
    
    * Bruker rxjava til konsumering av event publisher
    
    * Bruk applicator fra context

--- k-es-grpc/src/main/kotlin/no/ks/kes/grpc/GrpcAggregateRepository.kt
+++ k-es-grpc/src/main/kotlin/no/ks/kes/grpc/GrpcAggregateRepository.kt
@@ -2,16 +2,13 @@ package no.ks.kes.grpc
 
 import com.eventstore.dbclient.*
 import com.eventstore.dbclient.ExpectedRevision.expectedRevision
+import io.reactivex.rxjava3.core.Flowable
 import mu.KotlinLogging
 import no.ks.kes.grpc.GrpcEventUtil.isIgnorable
 import no.ks.kes.lib.*
 import no.ks.kes.lib.EventData
-import org.reactivestreams.Subscriber
-import org.reactivestreams.Subscription
 import java.util.*
-import java.util.concurrent.CompletableFuture
 import java.util.concurrent.ExecutionException
-import java.util.concurrent.TimeUnit
 import kotlin.reflect.KClass
 
 private val log = KotlinLogging.logger {}
@@ -20,19 +17,21 @@ class GrpcAggregateRepository(
     private val eventStoreDBClient: EventStoreDBClient,
     private val serdes: EventSerdes,
     private val streamIdGenerator: (aggregateType: String, aggregateId: UUID) -> String,
-    private val metadataSerdes: EventMetadataSerdes<out Metadata>? = null
+    private val metadataSerdes: EventMetadataSerdes<out Metadata>? = null,
 ) : AggregateRepository() {
 
     override fun append(aggregateType: String, aggregateId: UUID, expectedEventNumber: ExpectedEventNumber, eventWrappers: List<Event<*>>) {
         val streamId = streamIdGenerator.invoke(aggregateType, aggregateId)
         try {
             val events = eventWrappers.map { toEventData(it, serdes) }
-            eventStoreDBClient.appendToStream(
-                streamId,
-                AppendToStreamOptions.get().expectedRevision(resolveExpectedRevision(expectedEventNumber)),
-                events.iterator()
-            )
-                .get().also {
+            eventStoreDBClient
+                .appendToStream(
+                    streamId,
+                    AppendToStreamOptions.get().expectedRevision(resolveExpectedRevision(expectedEventNumber)),
+                    events.iterator()
+                )
+                .get()
+                .also {
                     log.info("wrote ${eventWrappers.size} events to stream ${streamId}, next expected version for this stream is ${it.nextExpectedRevision}")
                 }
         } catch (e: ExecutionException) {
@@ -56,26 +55,6 @@ class GrpcAggregateRepository(
             }
         }.build()
 
-    override fun getSerializationId(eventDataClass: KClass<EventData<*>>): String =
-        serdes.getSerializationId(eventDataClass)
-
-    override fun <A : Aggregate> read(aggregateId: UUID, aggregateType: String, applicator: (state: A?, event: EventWrapper<*>) -> A?): AggregateReadResult {
-        val streamId = streamIdGenerator.invoke(aggregateType, aggregateId)
-
-        return AggregateSubscriber(serdes, metadataSerdes, aggregateId, streamId, applicator)
-            .apply {
-                eventStoreDBClient.readStreamReactive(
-                    streamId,
-                    ReadStreamOptions.get()
-                        .forwards()
-                        .fromStart()
-                        .notResolveLinkTos()
-                ).subscribe(this)
-            }
-            .future
-            .get(5, TimeUnit.MINUTES)
-    }
-
     private fun resolveExpectedRevision(expectedEventNumber: ExpectedEventNumber): ExpectedRevision =
         when (expectedEventNumber) {
             is ExpectedEventNumber.AggregateDoesNotExist -> ExpectedRevision.noStream()
@@ -84,70 +63,111 @@ class GrpcAggregateRepository(
             is ExpectedEventNumber.Exact -> expectedRevision(expectedEventNumber.eventNumber)
         }
 
-}
+    override fun getSerializationId(eventDataClass: KClass<EventData<*>>): String =
+        serdes.getSerializationId(eventDataClass)
 
-private class AggregateSubscriber<A : Aggregate>(
-    private val serdes: EventSerdes,
-    private val metadataSerdes: EventMetadataSerdes<out Metadata>?,
-    private val aggregateId: UUID,
-    private val streamId: String,
-    private val applicator: (state: A?, event: EventWrapper<*>) -> A?
-) : Subscriber<ReadMessage> {
+    override fun <A : Aggregate> read(aggregateId: UUID, aggregateType: String, applicator: (state: A?, event: EventWrapper<*>) -> A?): AggregateReadResult {
+        val streamId = streamIdGenerator.invoke(aggregateType, aggregateId)
 
-    val future = CompletableFuture<AggregateReadResult>()
-
-    private var subscription: Subscription? = null
-    private var state: A? = null
-    private var lastEventNumber: Long? = null
-
-    override fun onSubscribe(subscription: Subscription) {
-        this.subscription = subscription
-        subscription.request(Long.MAX_VALUE)
-    }
-
-    override fun onNext(message: ReadMessage) {
-        if (message.isIgnorable()) {
-            lastEventNumber = message.event.event.revision
-        } else {
-            val eventMeta =
-                if (message.event.event.userMetadata.isNotEmpty() && metadataSerdes != null)
-                    metadataSerdes.deserialize(message.event.event.userMetadata)
-                else null
-            val eventData = EventUpgrader.upgrade(serdes.deserialize(message.event.event.eventData, message.event.event.eventType))
-            state = applicator.invoke(
-                state,
-                EventWrapper(
-                    Event(
-                        aggregateId = aggregateId,
-                        eventData = eventData,
-                        metadata = eventMeta
-                    ),
-                    eventNumber = message.event.event.revision,
-                    serializationId = serdes.getSerializationId(eventData::class)
+        return try {
+            Flowable
+                .fromPublisher(
+                    eventStoreDBClient.readStreamReactive(
+                        streamId,
+                        ReadStreamOptions.get()
+                            .forwards()
+                            .fromStart()
+                            .notResolveLinkTos()
+                    )
                 )
+                .reduce(AggregateContext(aggregateId, streamId, applicator)) { aggregate, message ->
+                    handleMessage(aggregate, message)
+                }
+                .blockingGet()
+                .let { toAggregateReadResult(it.state, it.lastStreamPosition, streamId) }
+        } catch (e: Exception) {
+            when (e) {
+                is StreamNotFoundException -> AggregateReadResult.NonExistingAggregate
+                else -> throw e
+            }
+        }
+    }
+
+    private fun <A : Aggregate> handleMessage(context: AggregateContext<A>, message: ReadMessage): AggregateContext<A> {
+        log.trace { "handleMessage: ${message.toLogString()}" }
+        return if (message.hasEvent()) {
+            context.apply {
+                state = handleEvent(context, message.event)
+                lastStreamPosition = message.event?.event?.revision ?: context.lastStreamPosition
+            }
+        } else {
+            log.debug { "Message does not have event (context: $context)" }
+            context
+        }
+    }
+
+    private fun <A : Aggregate> handleEvent(context: AggregateContext<A>, event: ResolvedEvent): A? =
+        if (!event.isIgnorable()) {
+            handleEvent(context, event.event)
+        } else {
+            context.state
+        }
+
+    private fun <A : Aggregate> handleEvent(context: AggregateContext<A>, event: RecordedEvent): A? {
+        val metadata = getMetadata(event)
+        val eventData = EventUpgrader.upgrade(serdes.deserialize(event.eventData, event.eventType))
+        return context.applicator.invoke(
+            context.state,
+            EventWrapper(
+                Event(
+                    aggregateId = context.aggregateId,
+                    eventData = eventData,
+                    metadata = metadata
+                ),
+                eventNumber = event.revision,
+                serializationId = serdes.getSerializationId(eventData::class)
             )
-            lastEventNumber = message.event.event.revision
+        )
+    }
+
+    private fun getMetadata(event: RecordedEvent) =
+        if (event.userMetadata.isNotEmpty() && metadataSerdes != null)
+            metadataSerdes.deserialize(event.userMetadata)
+        else
+            null
+
+    private fun ReadMessage.toLogString() = "ReadMessage(" +
+            "firstStreamPosition=${if (hasFirstStreamPosition()) firstStreamPosition else null}, " +
+            "lastStreamPosition=${if (hasLastStreamPosition()) lastStreamPosition else null}, " +
+            "lastAllPosition=${if (hasLastAllPosition()) lastStreamPosition else null}, " +
+            "event=${if (hasEvent()) event else null}" +
+            ")"
+
+    private fun <A : Aggregate> toAggregateReadResult(state: A?, lastStreamPosition: Long?, streamId: String): AggregateReadResult {
+        return if (lastStreamPosition != null) {
+            toAggregateReadResult(state, lastStreamPosition)
+        } else {
+            // When the aggregate stream has no events
+            throw RuntimeException("Error reading ${streamId}, the stream exists but does not contain any events")
         }
     }
 
-    override fun onError(throwable: Throwable) {
-        when (throwable) {
-            is StreamNotFoundException -> future.complete(AggregateReadResult.NonExistingAggregate)
-            else -> future.completeExceptionally(throwable)
+    private fun <A : Aggregate> toAggregateReadResult(state: A?, lastStreamPosition: Long): AggregateReadResult {
+        return if (state == null) {
+            // When the aggregate stream has events, but applying these did not lead to a initialized state
+            AggregateReadResult.UninitializedAggregate(lastStreamPosition)
+        } else {
+            // When the aggregate stream has events, and applying these has lead to a initialized state
+            AggregateReadResult.InitializedAggregate(state, lastStreamPosition)
         }
     }
-
-    override fun onComplete() {
-        when {
-            //when the aggregate stream has events, but applying these did not lead to a initialized state
-            state == null && lastEventNumber != null -> future.complete(AggregateReadResult.UninitializedAggregate(lastEventNumber!!))
-
-            //when the aggregate stream has events, and applying these has lead to a initialized state
-            state != null && lastEventNumber != null -> future.complete(AggregateReadResult.InitializedAggregate(state!!, lastEventNumber!!))
-
-            //when the aggregate stream has no events
-            else -> future.completeExceptionally(RuntimeException("Error reading $streamId, the stream exists but does not contain any events"))
-        }
-    }
-
 }
+
+private data class AggregateContext<A : Aggregate>(
+    val aggregateId: UUID,
+    val streamId: String,
+    val applicator: (state: A?, event: EventWrapper<*>) -> A?,
+    var state: A? = null,
+    var lastStreamPosition: Long? = null,
+)
+
