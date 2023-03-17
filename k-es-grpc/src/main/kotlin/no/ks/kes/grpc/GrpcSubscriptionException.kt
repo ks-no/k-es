a770e880a30cd99f6413343e9fd4c449986dd408
Audun Meltzer <audun.meltzer@gmail.com>
2023-03-17 21:30:45 +0100

    Ikke nå lengre

--- k-es-grpc/src/main/kotlin/no/ks/kes/grpc/GrpcSubscriptionException.kt
+++ k-es-grpc/src/main/kotlin/no/ks/kes/grpc/GrpcSubscriptionException.kt
@@ -1,9 +1,6 @@
 package no.ks.kes.grpc
 
 
-/**
- * Exception thrown when subscription is dropped
- */
 class GrpcSubscriptionException(val reason: GrpcSubscriptionReason, cause: Exception?) :
     RuntimeException("Subscription was dropped. Reason: $reason", cause) {
     constructor(reason: GrpcSubscriptionReason) : this(reason, null)
